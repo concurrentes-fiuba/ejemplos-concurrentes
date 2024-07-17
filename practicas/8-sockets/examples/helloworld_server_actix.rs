d13fbc4d2211712526c8373fa1f3e390de4a08f6
Ariel Scarpinelli <triforcexp@gmail.com>
2024-07-16 23:11:31 -0300

    remueve ejemplo tcp con mutex

--- practicas/8-sockets/examples/helloworld_server_actix.rs
+++ practicas/8-sockets/examples/helloworld_server_actix.rs
@@ -1,17 +1,13 @@
 use std::net::SocketAddr;
-use std::sync::Arc;
 
-use actix::{Actor, ActorContext, ActorFutureExt, AsyncContext, Context, ContextFutureSpawner, StreamHandler};
-use actix::fut::wrap_future;
-use tokio::io;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, split, WriteHalf};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::Mutex;
+use actix::{Actor, ActorFutureExt, Context, ContextFutureSpawner, StreamHandler, WrapFuture};
+use tokio::io::{AsyncWriteExt, WriteHalf, split, BufReader, AsyncBufReadExt};
+use tokio::net::TcpStream;
+use tokio::net::TcpListener;
 use tokio_stream::wrappers::LinesStream;
 
 struct HelloServer {
-    // write: Option<WriteHalf<TcpStream>>,
-    write: Arc<Mutex<WriteHalf<TcpStream>>>,
+    write: Option<WriteHalf<TcpStream>>,
     addr: SocketAddr,
 }
 
@@ -19,46 +15,23 @@ impl Actor for HelloServer {
     type Context = Context<Self>;
 }
 
-/*
 impl StreamHandler<Result<String, std::io::Error>> for HelloServer {
     fn handle(&mut self, read: Result<String, std::io::Error>, ctx: &mut Self::Context) {
         if let Ok(line) = read {
             println!("[{:?}] Hello {}", self.addr, line);
             let mut write = self.write.take()
                 .expect("No debería poder llegar otro mensaje antes de que vuelva por usar ctx.wait");
-            wrap_future::<_, Self>(async move {
+            async move {
                 write
                     .write_all(format!("Hello {}\n", line).as_bytes()).await
                     .expect("should have sent");
                 write
-            })
+            }.into_actor(self)
                 .map(|write, this, _| this.write = Some(write))
                 .wait(ctx);
         } else {
             println!("[{:?}] Failed to read line {:?}", self.addr, read);
         }
-    }
-}
- */
-
-impl StreamHandler<Result<String, std::io::Error>> for HelloServer {
-    fn handle(&mut self, read: Result<String, std::io::Error>, ctx: &mut Self::Context) {
-        if let Ok(line) = read {
-            println!("[{:?}] Hello {}", self.addr, line);
-            let arc = self.write.clone();
-            wrap_future::<_, Self>(async move {
-                arc.lock().await
-                    .write_all(format!("Hello {}\n", line).as_bytes()).await
-                    .expect("should have sent")
-            }).spawn(ctx);
-        } else {
-            println!("[{:?}] Failed to read line {:?}", self.addr, read);
-        }
-    }
-
-    fn finished(&mut self, ctx: &mut Self::Context) {
-        println!("[{:?}] desconectado", self.addr);
-        ctx.stop();
     }
 }
 
@@ -74,8 +47,7 @@ async fn main() {
         HelloServer::create(|ctx| {
             let (read, write_half) = split(stream);
             HelloServer::add_stream(LinesStream::new(BufReader::new(read).lines()), ctx);
-            let write =  Arc::new(Mutex::new(write_half));
-            //let write = Some(write_half);
+            let write = Some(write_half);
             HelloServer { addr, write }
         });
     }
